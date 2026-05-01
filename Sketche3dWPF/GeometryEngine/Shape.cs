23b5d274e8a7035011d6f73dd398488c15019260
latesh-munde <lateshmunde@gmail.com>
2026-05-01 22:53:53 +0530

    abstract shape class

---
+++ Sketche3dWPF/GeometryEngine/Shape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketche3dWPF.GeometryEngine
+{
+    public abstract class Shape
+    {
+        private string mType;
+        private string mName;
+
+        public Shape(string type, string name)
+        {
+            this.mType = type;
+            this.mName = name;
+        }
+
+        public string GetType() { return mType; }
+        public string GetName() { return mName; }
+
+        public abstract void save(StreamWriter sw);
+        
+
+
+    }
+}
