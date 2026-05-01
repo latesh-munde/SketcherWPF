3d8b567a5a123fac78f6132cbb857a268a9f04f5
latesh-munde <lateshmunde@gmail.com>
2026-05-01 23:36:19 +0530

    Cube file

---
+++ Sketche3dWPF/GeometryEngine/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketche3dWPF.GeometryEngine
+{
+    public class Cube : Shape
+    {
+        private double mSide;
+
+        public Cube (string name, double side) : base("cube", name)
+        {
+            this.mSide = side;
+        }
+
+        public double GetSide() { return mSide; }
+
+        public override void save(StreamWriter sw)
+        {
+            sw.Write(GetType()); sw.Write(" ");
+            sw.Write(GetName()); sw.Write(" ");
+            sw.Write(GetSide());
+        }
+    }
+}
