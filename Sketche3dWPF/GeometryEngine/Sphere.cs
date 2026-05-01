5de591376c2008496d011ca04693a2e4bbd56cfa
latesh-munde <lateshmunde@gmail.com>
2026-05-01 23:37:52 +0530

    Sphere file

---
+++ Sketche3dWPF/GeometryEngine/Sphere.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketche3dWPF.GeometryEngine
+{
+    public class Sphere : Shape
+    {
+        private double mRadius;
+
+        public Sphere(string name, double radius, double height): base("sphere", name)
+        {
+            this.mRadius = radius;
+        }
+
+        public double GetRadius() { return mRadius; }
+
+        public override void save(StreamWriter sw)
+        {
+            sw.Write(GetType()); sw.Write(" ");
+            sw.Write(GetName()); sw.Write(" ");
+            sw.Write(GetRadius()); 
+        }
+    }
+}
