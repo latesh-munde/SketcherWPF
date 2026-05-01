e1f65fe15a6d7c7d73fb15f9433eac410fa3f644
latesh-munde <lateshmunde@gmail.com>
2026-05-01 23:37:21 +0530

    Cylinder file

---
+++ Sketche3dWPF/GeometryEngine/Cylinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sketche3dWPF.GeometryEngine
+{
+    public class Cylinder : Shape
+    {
+        private double mRadius;
+        private double mHeight;
+
+        public Cylinder(string name, double radius, double height): base("cylinder", name)
+        {
+            this.mRadius = radius;
+            this.mHeight = height;
+        }
+
+        public double GetRadius() { return mRadius; }
+        public double GetHeight() { return mHeight; }
+
+        public override void save(StreamWriter sw)
+        {
+            sw.Write(GetType()); sw.Write(" ");
+            sw.Write(GetName()); sw.Write(" ");
+            sw.Write(GetRadius()); sw.Write(" ");
+            sw.Write(GetHeight());
+        }
+    }
+}
